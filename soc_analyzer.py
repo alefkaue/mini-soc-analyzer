44ca52136809b28cb1d3a18b3fdd35f5d259fd12
Alef Santos <167534821+alefkaue@users.noreply.github.com>
2026-03-16 21:26:41 -0300

    Update soc_analyzer.py

--- soc_analyzer.py
+++ soc_analyzer.py
@@ -1,2 +1,29 @@
-print("Mini SOC Analyzer iniciado")
+eventos = [
+    {"ip": "192.168.0.10", "evento": "login_sucesso"},
+    {"ip": "192.168.0.11", "evento": "login_falha"},
+    {"ip": "192.168.0.11", "evento": "login_falha"},
+    {"ip": "192.168.0.11", "evento": "login_falha"},
+    {"ip": "10.0.0.5",     "evento": "scan_porta"},
+    {"ip": "10.0.0.5",     "evento": "scan_porta"},
+    {"ip": "172.16.0.7",   "evento": "login_sucesso"},
+    {"ip": "8.8.8.8",      "evento": "acesso_externo"},
+]
+
+def contar_eventos(lista_eventos):
+    pass
+
+def identificar_bruteforce(lista_eventos):
+    pass
+
+def identificar_scanners(lista_eventos):
+    pass
+
+def listar_ips_unicos(lista_eventos):
+    pass
+
+def gerar_relatorio(lista_eventos):
+    pass
+
+if __name__ == "__main__":
+    gerar_relatorio(eventos)
 
